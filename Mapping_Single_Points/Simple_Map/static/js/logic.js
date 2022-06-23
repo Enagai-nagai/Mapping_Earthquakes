e5821f8d44c1c84e81e376e42f98954e0cd4078a
enagai <nagainagai.e@gmail.com>
2022-06-22 23:25:14 -0500

    adding

--- Mapping_Single_Points/Simple_Map/static/js/logic.js
+++ Mapping_Single_Points/Simple_Map/static/js/logic.js
@@ -3,7 +3,7 @@ console.log("working");
 
 // Create the map object with a center and zoom level.
 // latitude, longitude, zoom level
-let map = L.map('mapid').setView([40.7, -94.5],7);
+let map = L.map('mapid').setView([34.0522, -118.2437],7);
 
 // We create the tile layer that will be the background of our map.
 // id can be changed to the followings mapbox/streets-v11
@@ -20,3 +20,15 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/t
 });
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
+
+// Add a Marker to the Map for Los Angeles, California
+var marker = L.marker([34.0522, -118.2437]).addTo(map);
+
+// Add a Circle to the Map
+// Skill Drill
+L.circleMarker([34.0522, -118.2437], {
+    radius: 300,
+    color: "black",
+    fillColor: '#ffffa1',
+}).addTo(map);
+
